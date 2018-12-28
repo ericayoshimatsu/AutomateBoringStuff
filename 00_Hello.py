48421d5e144b62625085b4f168ad871473d4bbd3
EricaYoh <ericayoshimatsu@gmail.com>
2018-12-28 19:14:45 -0200

    Added files

---
+++ 00_Hello.py
@@ -0,0 +1,16 @@
+# This program says hello and asks for my name
+
+print("Hello World")
+
+print("What is your name?")
+myName = input()
+print("It's good to meet you, " + myName)
+print("The lenght of your name is: ")
+print(len(myName))
+
+print("What is your age?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
+
+
+      
